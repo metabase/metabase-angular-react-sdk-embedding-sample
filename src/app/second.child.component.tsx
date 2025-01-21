70e98a5e47261ca2beaace928a981019980c2dde
Roman Abdulmanov <roman.abdulmanov@gmail.com>
2025-01-21 17:47:38 +0000

    multiple components

---
+++ src/app/second.child.component.tsx
@@ -0,0 +1,8 @@
+import { InteractiveQuestion } from "@metabase/embedding-sdk-react";
+import * as React from "react";
+
+const SecondComponent = () => {
+  return <InteractiveQuestion questionId={107} />;
+};
+
+export default SecondComponent;
